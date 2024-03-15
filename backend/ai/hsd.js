b6a986738b24cb30bc506c2856ce919c58f2c00e
Karan <karanmandve1234@gmail.com>
2024-03-15 12:34:14 +0530

    fix hate speech detection completely

--- backend/ai/hsd.js
+++ backend/ai/hsd.js
@@ -1,4 +1,6 @@
 const fetch =require("node-fetch");
+const {google} = require('googleapis');
+
 async function detectHateSpeech(inputString) {
     // const url = 'https://fuk.ai/detect-hatespeech/';
     // const authToken = '8570fd1340a48e255a84dd852261416e9d946fb50d0daf6820e257b02dba3ca4';
@@ -17,21 +19,59 @@ async function detectHateSpeech(inputString) {
     //     return false;
     // }
 
-    const response = await fetch(
-		"https://api-inference.huggingface.co/models/Hate-speech-CNERG/indic-abusive-allInOne-MuRIL",
-		{
-			headers: { Authorization: "Bearer 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" },
-			method: "POST",
-			body: JSON.stringify("inputs: "+inputString),
-		}
-	);
-	const result = await response.json();
-    if(result[0][0].label == "LABEL_0"){
-        return false;
+
+    // const response = await fetch(
+	// 	"https://api-inference.huggingface.co/models/Hate-speech-CNERG/indic-abusive-allInOne-MuRIL",
+	// 	{
+	// 		headers: { Authorization: "Bearer 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" },
+	// 		method: "POST",
+	// 		body: JSON.stringify("inputs: "+inputString),
+	// 	}
+	// );
+	// const result = await response.json();
+    // if(result[0][0].label == "LABEL_0"){
+    //     return false;
+    // }
+    // else{
+    //     return true;
+    // }
+
+    API_KEY = 'AIzaSyB9aWLUzdDjK4nuwfdsdWy6nFNQFhWYrjk';
+    DISCOVERY_URL =
+    'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
+
+
+    try {
+        const client = await google.discoverAPI(DISCOVERY_URL);
+    
+        const analyzeRequest = {
+        comment: {
+            text: inputString,
+        },
+        languages: ["en", "hi-Latn"],
+        requestedAttributes: {
+            TOXICITY: {},
+        },
+        };
+    
+        const response = await client.comments.analyze({
+        key: API_KEY,
+        resource: analyzeRequest,
+        });
+    
+        // console.log(JSON.stringify(response.data, null, 2));
+        console.log(response.data.attributeScores.TOXICITY.summaryScore.value)
+        toxicity_no = response.data.attributeScores.TOXICITY.summaryScore.value;
+        if (toxicity_no >= 0.1){
+            return true
+        }else{
+            return false
+        }
+
+    } catch (err) {
+        throw err;
     }
-    else{
-        return true;
-    }
+
 	
 }
 
